2ebfac8e1a173118c7440e887a026982449d19f9
kodekito <kodekito@gmail.com>
2020-08-03 22:45:56 +0700

    Youtube Scraping example

---
+++ youtube.js
@@ -0,0 +1,42 @@
+const pup = require('puppeteer');
+// const pup = require('puppeteer-core');
+
+(async() => {
+    try {
+        const browser = await pup.launch({
+            headless: true,
+            executablePath: process.env.CHROMIUM_PATH,
+            args: ['--no-sandbox'],            
+            // headless: false,
+            // slowMo: 100
+        });
+        const page = await browser.newPage();
+        
+        await page.goto('https://www.youtube.com/');
+
+        await page.waitForSelector('#search');
+
+        await page.click('#search-container');
+        await page.keyboard.type('Onesinus SPT');
+        await page.keyboard.type(String.fromCharCode(13))
+
+        await page.waitForSelector("#avatar-section");
+        await page.click('#avatar-section');
+
+        await page.evaluate(() => {
+            const video_titles = document.querySelectorAll("a#video-title");
+            video_titles.forEach((title, idx) => {
+                console.log(video_titles[idx].textContent);
+            });
+            
+            // video_titles.map(title => {
+            //     console.log(title.textContent);
+            // })
+            
+
+        });
+        // await browser.close();
+    } catch (error) {
+        console.log(error);
+    }
+})();
